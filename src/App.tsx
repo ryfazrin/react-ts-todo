74d29ac3d615529cf65b5c429c23cc94fe886fe7
ryfazrin <riansera515@gmail.com>
2023-02-14 16:53:24 +0700

    added: state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 const App: React.FC = () => {
+  const [task, setTask] = useState<string>('')
+  const [deadline, setDeadLine] = useState<number>(0)
   
   return (
     <div className='App'>
       <div className="header">
         <div className="inputContainer">
-          <input type="text" name="task" placeholder="Add a task" />
-          <input type="number" name="deadline" placeholder="Set a deadline(in days)" />
+          <input type="text" value={task} name="task" placeholder="Add a task" />
+          <input type="number" value={deadline} name="deadline" placeholder="Set a deadline(in days)" />
         </div>
         <button>Add</button>
       </div>
